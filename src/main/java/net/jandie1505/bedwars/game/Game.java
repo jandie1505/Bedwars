jandie1505/Bedwars

commit 7acae4adcc15995585152129e6234c11b54537a1

new time action system

src/main/java/net/jandie1505/bedwars/game/Game.java
@@ -21,8 +21,10 @@ import net.jandie1505.bedwars.game.team.BedwarsTeam;
 import net.jandie1505.bedwars.game.team.TeamData;
 import net.jandie1505.bedwars.game.team.TeamUpgradesConfig;
 import net.jandie1505.bedwars.game.team.traps.BedwarsTrap;
-import net.jandie1505.bedwars.game.timeactions.*;
-import net.jandie1505.bedwars.lobby.setup.*;
+import net.jandie1505.bedwars.game.timeactions.base.TimeAction;
+import net.jandie1505.bedwars.game.timeactions.base.TimeActionData;
+import net.jandie1505.bedwars.game.timeactions.provider.TimeActionCreator;
+import net.jandie1505.bedwars.lobby.MapData;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.*;
@@ -42,11 +44,12 @@ import java.util.*;
 
 public class Game extends GamePart implements ManagedListener {
     private final World world;
-    private final GameConfig gameConfig;
+    private final MapData data;
     private final List<BedwarsTeam> teams;
     private final Map<UUID, PlayerData> players;
     private final List<Generator> generators;
     private final List<TimeAction> timeActions;
+    private final TimeActionCreator timeActionCreator;
     private final List<Location> playerPlacedBlocks;
     private final Map<UUID, Scoreboard> playerScoreboards;
     private final ItemShop itemShop;
@@ -61,28 +64,29 @@ public class Game extends GamePart implements ManagedListener {
     private BedwarsTeam winner;
     private boolean noWinnerEnd;
 
-    public Game(Bedwars plugin, World world, GameConfig gameConfig, List<TeamData> teams, List<GeneratorData> generators, List<LobbyGeneratorUpgradeTimeActionData> generatorUpgradeTimeActions, List<LobbyDestroyBedsTimeActionData> bedDestroyTimeActions, List<LobbyWorldborderChangeTimeActionData> worldborderChangeTimeActions, List<LobbyEndgameWitherTimeActionData> endgameWitherTimeActions, JSONObject shopConfig, ArmorConfig armorConfig, TeamUpgradesConfig teamUpgradesConfig) {
+    public Game(Bedwars plugin, World world, MapData data, JSONObject shopConfig, ArmorConfig armorConfig, TeamUpgradesConfig teamUpgradesConfig) {
         super(plugin);
         this.world = world;
-        this.gameConfig = gameConfig;
+        this.data = data;
         this.teams = Collections.synchronizedList(new ArrayList<>());
         this.players = Collections.synchronizedMap(new HashMap<>());
         this.generators = Collections.synchronizedList(new ArrayList<>());
         this.timeActions = Collections.synchronizedList(new ArrayList<>());
+        this.timeActionCreator = new TimeActionCreator(this);
         this.playerPlacedBlocks = Collections.synchronizedList(new ArrayList<>());
         this.playerScoreboards = Collections.synchronizedMap(new HashMap<>());
         this.itemShop = new ItemShop(this);
         this.armorConfig = armorConfig;
         this.teamUpgradesConfig = teamUpgradesConfig;
         this.managedEntities = Collections.synchronizedList(new ArrayList<>());
-        this.time = this.gameConfig.maxTime();
+        this.time = this.data.maxTime();
         this.publicEmeraldGeneratorLevel = 0;
         this.publicDiamondGeneratorLevel = 0;
         this.prepared = false;
         this.winner = null;
         this.noWinnerEnd = false;
 
-        for (TeamData teamData : List.copyOf(teams)) {
+        for (TeamData teamData : List.copyOf(this.data.teams())) {
             BedwarsTeam team = new BedwarsTeam(this, teamData);
 
             this.teams.add(team);
@@ -96,33 +100,25 @@ public class Game extends GamePart implements ManagedListener {
             }
         }
 
-        for (GeneratorData generatorData : generators) {
+        for (GeneratorData generatorData : this.data.globalGenerators()) {
             this.generators.add(new PublicGenerator(
                     this,
                     generatorData
             ));
         }
 
-        for (LobbyGeneratorUpgradeTimeActionData generatorUpgradeData : generatorUpgradeTimeActions) {
-
-            if (generatorUpgradeData.getGeneratorType() == 1) {
-                this.timeActions.add(new DiamondGeneratorUpgradeAction(this, generatorUpgradeData.getTime(), "§aDiamond Generators §ehave beed upgraded to §aLevel " + (generatorUpgradeData.getLevel() + 1), "Diamond " + (generatorUpgradeData.getLevel() + 1), generatorUpgradeData.getLevel()));
-            } else if (generatorUpgradeData.getGeneratorType() == 2) {
-                this.timeActions.add(new EmeraldGeneratorUpgradeAction(this, generatorUpgradeData.getTime(), "§aEmerald Generators §ehave beed upgraded to §aLevel " + (generatorUpgradeData.getLevel() + 1), "Emerald " + (generatorUpgradeData.getLevel() + 1), generatorUpgradeData.getLevel()));
+        for (TimeActionData timeActionData : this.data.timeActions()) {
+            try {
+                TimeAction timeAction = this.timeActionCreator.createTimeAction(timeActionData);
+                if (timeAction == null) {
+                    this.getPlugin().getLogger().warning("Couldn't create time action: Invalid type");
+                    continue;
+                }
+                this.timeActions.add(timeAction);
+            } catch (Exception e) {
+                this.getPlugin().getLogger().warning("Couldn't create time action: " + e.getMessage());
+                continue;
             }
-
-        }
-
-        for (LobbyDestroyBedsTimeActionData destroyBedsData : bedDestroyTimeActions) {
-            this.timeActions.add(new DestroyBedsAction(this, destroyBedsData.getTime(), destroyBedsData.isDisableBeds()));
-        }
-
-        for (LobbyWorldborderChangeTimeActionData worldborderChangeData : worldborderChangeTimeActions) {
-            this.timeActions.add(new WorldborderChangeTimeAction(this, worldborderChangeData.getTime(), worldborderChangeData.getChatMessage(), worldborderChangeData.getScoreboardText(), worldborderChangeData.getRadius()));
-        }
-
-        for (LobbyEndgameWitherTimeActionData endgameWitherSpawnData : endgameWitherTimeActions) {
-            this.timeActions.add(new EndgameWitherTimeAction(this, endgameWitherSpawnData.getTime()));
         }
 
         Collections.sort(this.timeActions);
@@ -141,8 +137,8 @@ public class Game extends GamePart implements ManagedListener {
 
         this.itemShop.initEntries(shopConfig);
 
-        this.world.getWorldBorder().setCenter(this.gameConfig.centerLocation());
-        this.world.getWorldBorder().setSize(this.gameConfig.mapRadius() * 2);
+        this.world.getWorldBorder().setCenter(this.data.centerLocation());
+        this.world.getWorldBorder().setSize(this.data.mapRadius() * 2);
 
         this.world.setGameRule(GameRule.DO_IMMEDIATE_RESPAWN, true);
         this.world.setGameRule(GameRule.ANNOUNCE_ADVANCEMENTS, false);
@@ -283,12 +279,12 @@ public class Game extends GamePart implements ManagedListener {
     private void teleportSpectatorsTask() {
 
         for (Player player : this.getPlugin().getServer().getOnlinePlayers()) {
-            if (player.getWorld() == this.gameConfig.centerLocation().getWorld()) continue;
+            if (player.getWorld() == this.data.centerLocation().getWorld()) continue;
 
             PlayerData playerData = this.getPlayers().get(player.getUniqueId());
             if (playerData != null) continue;
 
-            player.teleport(this.gameConfig.centerLocation());
+            player.teleport(this.data.centerLocation());
         }
 
     }
@@ -350,8 +346,8 @@ public class Game extends GamePart implements ManagedListener {
 
             if (playerData.isAlive()) {
 
-                if (playerData.getRespawnCountdown() != this.gameConfig.respawnCountdown()) {
-                    playerData.setRespawnCountdown(this.gameConfig.respawnCountdown());
+                if (playerData.getRespawnCountdown() != this.data.respawnCountdown()) {
+                    playerData.setRespawnCountdown(this.data.respawnCountdown());
                 }
 
                 if (!this.getPlugin().isPlayerBypassing(player.getUniqueId()) && player.getGameMode() != GameMode.SURVIVAL) {
@@ -665,8 +661,8 @@ public class Game extends GamePart implements ManagedListener {
 
         for (TimeAction timeAction : this.getTimeActions()) {
 
-            if (this.time <= timeAction.getTime() && !timeAction.isCompleted()) {
-                timeAction.execute();
+            if (this.time <= timeAction.getData().time() && !timeAction.isCompleted()) {
+                timeAction.run();
             }
 
         }
@@ -1365,7 +1361,7 @@ public class Game extends GamePart implements ManagedListener {
                 continue;
             }
 
-            int inTime = this.time - timeAction.getTime();
+            int inTime = this.time - timeAction.getData().time();
 
             sidebarDisplayStrings.add(timeAction.getScoreboardText() + " §rin §a" + Bedwars.getDurationFormat(inTime));
 
@@ -1604,8 +1600,8 @@ public class Game extends GamePart implements ManagedListener {
         return this.world;
     }
 
-    public GameConfig getGameConfig() {
-        return this.gameConfig;
+    public MapData getData() {
+        return this.data;
     }
 
     public List<BedwarsTeam> getTeams() {

src/main/java/net/jandie1505/bedwars/game/timeactions/actions/DestroyBedsAction.java
@@ -1,21 +1,28 @@
-package net.jandie1505.bedwars.game.timeactions;
+package net.jandie1505.bedwars.game.timeactions.actions;
 
 import net.jandie1505.bedwars.game.Game;
 import net.jandie1505.bedwars.game.player.PlayerData;
 import net.jandie1505.bedwars.game.team.BedwarsTeam;
+import net.jandie1505.bedwars.game.timeactions.base.TimeAction;
+import net.jandie1505.bedwars.game.timeactions.base.TimeActionData;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 
+import java.util.Objects;
+
 public class DestroyBedsAction extends TimeAction {
+    public static final TimeActionData.DataAccessor<Boolean> DISABLE_BEDS = new TimeActionData.DataAccessor<>("disableBeds");
     private final boolean disableBeds;
 
-    public DestroyBedsAction(Game game, int time, boolean disableBeds) {
-        super(game, time, "§cAll beds have been destroyed (replaceable=" + !disableBeds + ")", "Beds gone " + DestroyBedsAction.isBedReplaceable(disableBeds));
-        this.disableBeds = disableBeds;
+    public DestroyBedsAction(Game game, TimeActionData data) {
+        super(game, data);
+        this.disableBeds = Objects.requireNonNullElse(data.getDataField(DISABLE_BEDS), false);
     }
 
     @Override
-    protected void run() {
+    protected void onRun() {
         for (Player player : this.getGame().getPlugin().getServer().getOnlinePlayers()) {
 
             PlayerData playerData = this.getGame().getPlayers().get(player.getUniqueId());
@@ -50,6 +57,16 @@ public class DestroyBedsAction extends TimeAction {
         }
     }
 
+    @Override
+    public BaseComponent[] getMessage() {
+        return new BaseComponent[]{new TextComponent("§cAll beds have been destroyed." + (this.disableBeds ? " §cThey cannot be replaced" : ""))};
+    }
+
+    @Override
+    public String getScoreboardText() {
+        return "";
+    }
+
     public boolean isDisableBeds() {
         return this.disableBeds;
     }
@@ -61,4 +78,5 @@ public class DestroyBedsAction extends TimeAction {
             return "";
         }
     }
+
 }

src/main/java/net/jandie1505/bedwars/game/timeactions/actions/GeneratorUpgradeAction.java
@@ -1,0 +1,43 @@
+package net.jandie1505.bedwars.game.timeactions.actions;
+
+import net.jandie1505.bedwars.game.Game;
+import net.jandie1505.bedwars.game.timeactions.base.TimeAction;
+import net.jandie1505.bedwars.game.timeactions.base.TimeActionData;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Material;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
+
+public class GeneratorUpgradeAction extends TimeAction {
+    private static final TimeActionData.DataAccessor<String> DATA_GENERATOR_MATERIAL = new TimeActionData.DataAccessor<>("material");
+    private static final TimeActionData.DataAccessor<Integer> DATA_UPGRADE_LEVEL = new TimeActionData.DataAccessor<>("upgrade_level");
+    private final Material material;
+    private final int upgradeLevel;
+
+    public GeneratorUpgradeAction(Game game, TimeActionData data) {
+        super(game, data);
+        this.material = Objects.requireNonNull(Material.getMaterial(Objects.requireNonNull(this.getData().getDataField(DATA_GENERATOR_MATERIAL))));
+        this.upgradeLevel = Objects.requireNonNull(this.getData().getDataField(DATA_UPGRADE_LEVEL));
+    }
+
+    @Override
+    protected void onRun() {
+        this.getGame().setPublicDiamondGeneratorLevel(upgradeLevel);
+    }
+
+    @Override
+    public @Nullable BaseComponent[] getMessage() {
+        return new BaseComponent[]{TextComponent.fromLegacy("§b" + this.material.name() + " generators have been successfully upgraded to level " + this.upgradeLevel)};
+    }
+
+    @Override
+    public @Nullable String getScoreboardText() {
+        return this.material.name() + "S Level " + this.upgradeLevel;
+    }
+
+    public int getUpgradeLevel() {
+        return this.upgradeLevel;
+    }
+}

src/main/java/net/jandie1505/bedwars/game/timeactions/base/TimeActionData.java
@@ -1,0 +1,67 @@
+package net.jandie1505.bedwars.game.timeactions.base;
+
+import org.json.JSONObject;
+
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * Contains data about time actions.
+ * @param type time action type
+ * @param time time action time
+ * @param dataFields data fields
+ */
+public record TimeActionData(String type, int time, Map<String, Object> dataFields) {
+
+    public record DataAccessor<T>(String key) {}
+
+    public TimeActionData(String type, int time, Map<String, Object> dataFields) {
+        this.type = type;
+        this.time = time;
+        this.dataFields = Map.copyOf(dataFields);
+    }
+
+    /**
+     * Returns a specific data value with the specific type
+     * @param accessor data accessor
+     * @return value
+     * @param <T> type
+     */
+    @SuppressWarnings("unchecked")
+    public <T> T getDataField(DataAccessor<T> accessor) {
+        try {
+            return (T) dataFields.get(accessor.key());
+        } catch (ClassCastException e) {
+            return null;
+        }
+    }
+
+    public JSONObject serializeToJSON() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("type", type);
+        jsonObject.put("time", time);
+        jsonObject.put("data", new JSONObject(dataFields));
+        return jsonObject;
+    }
+
+    public static TimeActionData deserializeFromJSON(JSONObject json) {
+
+        try {
+            String type = json.getString("type");
+            int time = json.getInt("time");
+
+            Map<String, Object> dataFields = Map.of();
+            JSONObject dataObject = json.optJSONObject("data");
+            if (dataObject != null) {
+                dataFields = Objects.requireNonNull(Objects.requireNonNull(dataObject).toMap());
+            }
+
+            return new TimeActionData(type, time, dataFields);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+}

src/main/java/net/jandie1505/bedwars/EventListener.java
@@ -88,15 +88,15 @@ public class EventListener implements ManagedListener {
 
             // BLOCK PLACE PROTECTION
 
-            if (((Game) this.plugin.getGame()).getGameConfig().spawnBlockPlaceProtection() > 0 || ((Game) this.plugin.getGame()).getGameConfig().villagerBlockPlaceProtection() > 0) {
+            if (((Game) this.plugin.getGame()).getData().spawnBlockPlaceProtection() > 0 || ((Game) this.plugin.getGame()).getData().villagerBlockPlaceProtection() > 0) {
 
                 for (BedwarsTeam team : ((Game) this.plugin.getGame()).getTeams()) {
 
-                    if (((Game) this.plugin.getGame()).getGameConfig().spawnBlockPlaceProtection() > 0) {
+                    if (((Game) this.plugin.getGame()).getData().spawnBlockPlaceProtection() > 0) {
 
                         for (Location location : team.getData().spawnpoints()) {
 
-                            if (Bedwars.getBlockDistance(WorldUtils.locationWithWorld(location, ((Game) this.plugin.getGame()).getWorld()), event.getBlock().getLocation()) <= ((Game) this.plugin.getGame()).getGameConfig().spawnBlockPlaceProtection()) {
+                            if (Bedwars.getBlockDistance(WorldUtils.locationWithWorld(location, ((Game) this.plugin.getGame()).getWorld()), event.getBlock().getLocation()) <= ((Game) this.plugin.getGame()).getData().spawnBlockPlaceProtection()) {
                                 event.setCancelled(true);
                                 event.getPlayer().sendMessage("§cYou cannot place blocks here");
                                 return;
@@ -106,7 +106,7 @@ public class EventListener implements ManagedListener {
 
                     }
 
-                    if (((Game) this.plugin.getGame()).getGameConfig().villagerBlockPlaceProtection() > 0) {
+                    if (((Game) this.plugin.getGame()).getData().villagerBlockPlaceProtection() > 0) {
 
                         List<Location> villagerLocations = new ArrayList<>();
                         villagerLocations.addAll(team.getData().shopVillagerLocations().stream().map(immutableLocation -> WorldUtils.locationWithWorld(immutableLocation, ((Game) this.getGame()).getWorld())).toList());
@@ -114,7 +114,7 @@ public class EventListener implements ManagedListener {
 
                         for (Location location : villagerLocations) {
 
-                            if (Bedwars.getBlockDistance(location, event.getBlock().getLocation()) <= ((Game) this.plugin.getGame()).getGameConfig().villagerBlockPlaceProtection()) {
+                            if (Bedwars.getBlockDistance(location, event.getBlock().getLocation()) <= ((Game) this.plugin.getGame()).getData().villagerBlockPlaceProtection()) {
                                 event.setCancelled(true);
                                 event.getPlayer().sendMessage("§cYou cannot place blocks here");
                                 return;
@@ -1214,7 +1214,7 @@ public class EventListener implements ManagedListener {
 
             for (MapData map : ((Lobby) this.plugin.getGame()).getMaps()) {
 
-                if (map.getWorld().equals(lore.get(1))) {
+                if (map.world().equals(lore.get(1))) {
 
                     event.getWhoClicked().closeInventory();
 
@@ -1226,7 +1226,7 @@ public class EventListener implements ManagedListener {
                     } else {
 
                         playerData.setVote(map);
-                        event.getWhoClicked().sendMessage("§aYou changed your vote to " + map.getName());
+                        event.getWhoClicked().sendMessage("§aYou changed your vote to " + map.world());
 
                     }
 
@@ -1995,7 +1995,7 @@ public class EventListener implements ManagedListener {
             }
 
             if (((Lobby) this.plugin.getGame()).getSelectedMap() != null) {
-                event.setMotd(((Lobby) this.plugin.getGame()).getSelectedMap().getName());
+                event.setMotd(((Lobby) this.plugin.getGame()).getSelectedMap().world());
             } else {
                 event.setMotd("MAP VOTING");
             }

commit e5a3292055b059a0e4582e35826d4c62e5cebda0

fixed timeactions not working (fixes most of the damage 7acae4adcc15995585152129e6234c11b54537a1 caused)

pom.xml
@@ -18,7 +18,7 @@
         <repository>
             <id>ChaosSquad-Repository</id>
             <name>ChaosSquad Repository</name>
-            <url>https://chaossquad.net:8443/snapshots</url>
+            <url>https://maven.chaossquad.net/snapshots</url>
         </repository>
         <repository>
             <id>spigot-repo</id>

src/main/java/net/jandie1505/bedwars/game/Game.java
@@ -42,6 +42,7 @@ import org.bukkit.scoreboard.*;
 import org.json.JSONObject;
 
 import java.util.*;
+import java.util.logging.Level;
 
 public class Game extends GamePart implements ManagedListener {
     private final World world;
@@ -119,7 +120,7 @@ public class Game extends GamePart implements ManagedListener {
                 }
                 this.timeActions.add(timeAction);
             } catch (Exception e) {
-                this.getPlugin().getLogger().warning("Couldn't create time action: " + e.getMessage());
+                this.getPlugin().getLogger().log(Level.WARNING, "Couldn't create time action", e);
                 continue;
             }
         }

src/main/java/net/jandie1505/bedwars/game/timeactions/actions/DestroyBedsAction.java
@@ -64,7 +64,7 @@ public class DestroyBedsAction extends TimeAction {
 
     @Override
     public String getScoreboardText() {
-        return "";
+        return "Beds gone";
     }
 
     public boolean isDisableBeds() {

src/main/java/net/jandie1505/bedwars/game/timeactions/actions/GeneratorUpgradeAction.java
@@ -11,30 +11,45 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Objects;
 
 public class GeneratorUpgradeAction extends TimeAction {
-    private static final TimeActionData.DataAccessor<String> DATA_GENERATOR_MATERIAL = new TimeActionData.DataAccessor<>("material");
-    private static final TimeActionData.DataAccessor<Integer> DATA_UPGRADE_LEVEL = new TimeActionData.DataAccessor<>("upgrade_level");
-    private final Material material;
+    private static final TimeActionData.DataAccessor<Integer> DATA_GENERATOR_MATERIAL = new TimeActionData.DataAccessor<>("generatorType");
+    private static final TimeActionData.DataAccessor<Integer> DATA_UPGRADE_LEVEL = new TimeActionData.DataAccessor<>("generatorLevel");
+    private final int generatorType;
     private final int upgradeLevel;
 
     public GeneratorUpgradeAction(Game game, TimeActionData data) {
         super(game, data);
-        this.material = Objects.requireNonNull(Material.getMaterial(Objects.requireNonNull(this.getData().getDataField(DATA_GENERATOR_MATERIAL))));
+        this.generatorType = Objects.requireNonNull(this.getData().getDataField(DATA_GENERATOR_MATERIAL));
         this.upgradeLevel = Objects.requireNonNull(this.getData().getDataField(DATA_UPGRADE_LEVEL));
     }
 
     @Override
     protected void onRun() {
-        this.getGame().setPublicDiamondGeneratorLevel(upgradeLevel);
+
+        switch (this.generatorType) {
+            case 1 -> this.getGame().setPublicDiamondGeneratorLevel(upgradeLevel);
+            case 2 -> this.getGame().setPublicEmeraldGeneratorLevel(upgradeLevel);
+        }
+
+    }
+
+    private String getMaterialName() {
+
+        return switch (this.generatorType) {
+            case 1 -> "Diamond";
+            case 2 -> "Emerald";
+            default -> "unknown";
+        };
+
     }
 
     @Override
     public @Nullable BaseComponent[] getMessage() {
-        return new BaseComponent[]{TextComponent.fromLegacy("§b" + this.material.name() + " generators have been successfully upgraded to level " + this.upgradeLevel)};
+        return new BaseComponent[]{TextComponent.fromLegacy("§b" + this.getMaterialName() + " generators have been successfully upgraded to level " + this.upgradeLevel)};
     }
 
     @Override
     public @Nullable String getScoreboardText() {
-        return this.material.name() + "S Level " + this.upgradeLevel;
+        return this.getMaterialName() + "s Level " + this.upgradeLevel;
     }
 
     public int getUpgradeLevel() {

src/main/java/net/jandie1505/bedwars/game/timeactions/base/TimeActionData.java
@@ -47,14 +47,10 @@ public record TimeActionData(String type, int time, Map<String, Object> dataFiel
     public static TimeActionData deserializeFromJSON(JSONObject json) {
 
         try {
-            String type = json.getString("type");
-            int time = json.getInt("time");
+            String type = (String) json.remove("type");
+            int time = (Integer) json.remove("time");
 
-            Map<String, Object> dataFields = Map.of();
-            JSONObject dataObject = json.optJSONObject("data");
-            if (dataObject != null) {
-                dataFields = Objects.requireNonNull(Objects.requireNonNull(dataObject).toMap());
-            }
+            Map<String, Object> dataFields = json.toMap();
 
             return new TimeActionData(type, time, dataFields);
         } catch (Exception e) {