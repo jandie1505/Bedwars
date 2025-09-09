7daf79d35265ba2051caa41a023baccdaea41b03
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2025-09-09 13:23:38 +0200

    added LobbyPlayersAddSubcommand

--- src/main/java/net/jandie1505/bedwars/game/lobby/commands/LobbyPlayersSubcommand.java
+++ src/main/java/net/jandie1505/bedwars/game/lobby/commands/LobbyPlayersSubcommand.java
@@ -4,6 +4,7 @@ import net.chaossquad.mclib.command.SubcommandCommand;
 import net.chaossquad.mclib.command.SubcommandEntry;
 import net.jandie1505.bedwars.constants.Permissions;
 import net.jandie1505.bedwars.game.lobby.Lobby;
+import net.jandie1505.bedwars.game.lobby.commands.players.LobbyPlayersAddSubcommand;
 import net.jandie1505.bedwars.game.lobby.commands.players.LobbyPlayersListSubcommand;
 import net.jandie1505.bedwars.game.lobby.commands.players.LobbyPlayersRemoveSubcommand;
 import net.jandie1505.bedwars.game.lobby.commands.players.LobbyPlayersValueSubcommand;
@@ -17,6 +18,7 @@ public class LobbyPlayersSubcommand extends SubcommandCommand {
         this.addSubcommand("remove", SubcommandEntry.of(new LobbyPlayersRemoveSubcommand(lobby)));
         this.addSubcommand("list", SubcommandEntry.of(new LobbyPlayersListSubcommand(lobby)));
         this.addSubcommand("value", SubcommandEntry.of(new LobbyPlayersValueSubcommand(lobby)));
+        this.addSubcommand("add", SubcommandEntry.of(new LobbyPlayersAddSubcommand(lobby)));
     }
 
 }
